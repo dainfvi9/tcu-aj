beb599a35015c25d73ee661ca5da46a5ebca66ab
donchoko <oscaralbertogutierrez@live.com>
2017-05-22 00:29:42 -0600

    0.0.5.2
    +Added create user UI
    +Added service to get school objects
    +Added other stuff
    
    *No creo que ninguno lea estos commits. Solo quiero decir que es media noche y tengo ganas de una cerveza pero es domingo y ya nadie vende a esta hora.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,20 @@ import { HttpModule } from '@angular/http';
 import { RouterModule} from '@angular/router';
 //Angular Material
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MdCardModule, MdToolbarModule, MdSidenavModule, MdButtonModule, MdCheckboxModule, MdInputModule } from '@angular/material';
+import { MdCardModule, MdToolbarModule, MdSidenavModule, MdButtonModule, 
+  MdSelectModule, MdCheckboxModule, MdInputModule } from '@angular/material';
 import 'hammerjs';
 import { AppComponent } from './app.component';
 import { LoginComponent } from '../login/login.component';
-
+import { UserCreateComponent } from '../users/create/user-create.component';
+import 'rxjs/add/operator/toPromise';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    UserCreateComponent
   ],
   imports: [
     BrowserModule,
@@ -28,10 +31,15 @@ import { LoginComponent } from '../login/login.component';
     MdToolbarModule,
     MdInputModule,
     MdCardModule,
+    MdSelectModule,
     RouterModule.forRoot([
       {
-        path:'',
+        path:'login',
         component: LoginComponent
+      },
+      {
+        path:'',
+        component: UserCreateComponent
       },
     ])
   ],
